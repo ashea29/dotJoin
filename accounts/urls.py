a547c683df473f85a33350f4725423d24d9cb565
ashea27 <shea.andrew27@gmail.com>
2020-02-25 06:50:55 -0700

    fixed signup issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(
          template_name = 'thanks.html'), name='logout'),
     # Sign Up
-    path("register/", views.UserRegister.as_view(), name="register")
+    path("register/", views.UserRegister.as_view(
+         template_name = 'accounts/register.html'), name="register")
 ]
