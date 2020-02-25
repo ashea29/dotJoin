020871ee5918a2225dd28df9d23bf5297c49b15d
ashea27 <shea.andrew27@gmail.com>
2020-02-25 09:47:38 -0700

    created form template, and added CRUD functionality

--- projects/urls.py
+++ projects/urls.py
@@ -3,18 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
-    # path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
-    # path('artists/new', views.artist_create, name='artist_create'),
-    # path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'),
-    # path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
-
-
-    # path('songs/', views.song_list, name='song_list'),
-    # path('songs/<int:pk>', views.song_detail, name='song_detail'),
-    # path('songs/new', views.song_create, name='song_create'),
-    # path('songs/<int:pk>/edit', views.song_edit, name='song_edit'),
-    # path('songs/<int:pk>/delete', views.song_delete, name='song_delete'),
-
-    # path('favorites/<int:song_id>/create', views.add_favorite, name='add_favorite'),
-    # path('favorites/<int:song_id>/remove', views.remove_favorite, name='remove_favorite')
+    path('projects/new', views.project_create, name='project_create'),
+    path('projects/<int:pk>/edit', views.project_edit, name='project_edit'),
+    path('projects/<int:pk>/delete', views.project_delete, name='project_delete'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,86 +2,42 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Project, Task
-# from .forms import ArtistForm, SongForm
+from .forms import ProjectForm
 
 
 # Create your views here.
+@login_required
 def project_list(request):
     projects = Project.objects.all()
     return render(request, 'projects/project_list.html', {'projects': projects})
 
-def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, 'projects/project_list.html', {'tasks': tasks})
-# def artist_detail(request, pk):
-#     artist = Artist.objects.get(id=pk)
-#     return render(request, 'tunr/artist_detail.html', {'artist': artist})
+
+@login_required
+def project_create(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save()
+            return redirect('project_list', pk=project.pk)
+    else:
+        form = ProjectForm()
+    return render(request, 'projects/project_form.html', {'form': form})
 
 
-# @login_required
-# def artist_create(request):
-#     if request.method == 'POST':
-#         form = ArtistForm(request.POST)
-#         if form.is_valid():
-#             artist = form.save()
-#             return redirect('artist_detail', pk=artist.pk)
-#     else:
-#         form = ArtistForm()
-#     return render(request, 'tunr/artist_form.html', {'form': form})
+@login_required
+def project_edit(request, pk):
+    project = Project.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            project = form.save()
+            return redirect('project_list', pk=project.pk)
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'projects/project_form.html', {'form': form})
 
 
-# @login_required
-# def artist_edit(request, pk):
-#     artist = Artist.objects.get(pk=pk)
-#     if request.method == "POST":
-#         form = ArtistForm(request.POST, instance=artist)
-#         if form.is_valid():
-#             artist = form.save()
-#             return redirect('artist_detail', pk=artist.pk)
-#     else:
-#         form = ArtistForm(instance=artist)
-#     return render(request, 'tunr/artist_form.html', {'form': form})
-
-
-# @login_required
-# def artist_delete(request, pk):
-#     Artist.objects.get(id=pk).delete()
-#     return redirect('artist_list')
-
-
-# def song_list(request):
-#     songs = Song.objects.all()
-#     return render(request, 'tunr/song_list.html', {'songs': songs})
-
-
-# def song_detail(request, pk):
-#     song = Song.objects.get(id=pk)
-#     return render(request, 'tunr/song_detail.html', {'song': song})
-
-
-# def song_create(request):
-#     if request.method == 'POST':
-#         form = SongForm(request.POST)
-#         if form.is_valid():
-#             song = form.save()
-#             return redirect('song_detail', pk=song.pk)
-#     else:
-#         form = SongForm()
-#     return render(request, 'tunr/song_form.html', {'form': form})
-
-
-# def song_edit(request, pk):
-#     song = Song.objects.get(pk=pk)
-#     if request.method == "POST":
-#         form = SongForm(request.POST, instance=song)
-#         if form.is_valid():
-#             artist = form.save()
-#             return redirect('song_detail', pk=song.pk)
-#     else:
-#         form = SongForm(instance=song)
-#     return render(request, 'tunr/song_form.html', {'form': form})
-
-
-# def song_delete(request, pk):
-#     Song.objects.get(id=pk).delete()
-#     return redirect('song_list')
+@login_required
+def project_delete(request, pk):
+    Project.objects.get(id=pk).delete()
+    return redirect('project_list')
